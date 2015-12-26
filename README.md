05c224c68d29af50e72e1142517e93c788cb42c5
Igor Chubin <igor@chub.in>
2015-12-27 00:22:13 +0200

    checked wttr.in in

--- README.md
+++ README.md
@@ -1,2 +1,150 @@
-# wttr.in
-Web frontend for wego
+## Usage
+
+
+You can access the service from a shell or from a Web browser:
+
+    $ curl wttr.in
+    Weather for City: Paris, France
+
+         \   /     Clear
+          .-.      10 – 11 °C     
+       ― (   ) ―   ↑ 11 km/h      
+          `-’      10 km          
+         /   \     0.0 mm         
+
+
+You can specify the location, for that you want to get the weather information.
+If you omit the location name, you will get the information for you current location,
+based on your IP address.
+
+    $ curl wttr.in/London
+    $ curl wttr.in/Moscow
+
+You can use 3-letters airport codes if you want to get the weather information
+about some airports:
+
+    $ curl wttr.in/muc      # Weather for IATA: muc, Munich International Airport, Germany
+    $ curl wttr.in/ham      # Weather for IATA: muc, Hamburg Airport, Germany
+
+You can also use IP-addresses (direct) or domain names (prefixed with @)
+as a location specificator:
+
+    $ curl wttr.in/@github.com
+    $ curl wttr.in/@msu.ru
+
+To get this information online, you can access the `:help` page:
+
+    $ curl wttr.in/:help
+
+## Installation 
+
+To install the program you need:
+
+1. Install external depndencies
+2. Install python dependencies used by the service
+3. Get WorldWeatherOnline API Key
+4. Configure wego
+5. Configure wttr.in
+6. Configure HTTP-frontend service
+
+### Install external dependencies
+
+External requirements:
+
+* wego [https://github.com/schachmat/wego], weather client for terminal
+
+To install `wego` you must have golang installed. After that:
+
+    go get https://github.com/schachmat/wego
+    go install https://github.com/schachmat/wego
+
+### Install python dependencies
+
+Python requirements:
+
+* Flask
+* geoip2
+* geopy
+* requests
+* gevent
+
+You can install them using `pip`. 
+
+If `virtualenv` is used:
+
+    virtualenv ve
+    ve/bin/pip install -r requirements.txt
+    ve/bin/pip bin/srv.py
+
+Also, you need to install the geoip2 database.
+You can use a free database GeoLite2, that can be downloaded from http://dev.maxmind.com/geoip/geoip2/geolite2/
+
+### Get WorldWeatherOnline key
+
+To get the WorldWeatherOnline API key, you must register here:
+ 
+    https://developer.worldweatheronline.com/auth/register
+
+### Configure wego
+
+After you have the key, configure `wego`:
+
+    $ cat ~/.wegorc 
+    {
+        "APIKey": "00XXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "City": "London",
+        "Numdays": 3,
+        "Imperial": false,
+        "Lang": "en"
+    }
+
+The `City` parameter in `~/.wegorc` is ignored.
+
+### Configure wttr.in
+
+Edit the `bin/srv.py` and specify the path to the local `wttr.in` installation,
+to the GeoLite database and to the `wego` installation:
+
+    MYDIR = "/home/igor/wttr.in"
+    GEOLITE = "/home/igor/wttr.in/GeoLite2-City.mmdb"
+    WEGO = "/home/igor/go/bin/wego"
+
+
+### Configure HTTP-frontend service
+
+Configure the web server, that will be used
+to access the service (if you want to use a web frontend; it's recommended):
+
+    server {
+        listen [::]:80;
+        server_name  wttr.in *.wttr.in;
+        access_log  /var/log/nginx/wttr.in-access.log  main;
+        error_log  /var/log/nginx/wttr.in-error.log;
+
+        location /clouds_files { root /var/www/igor/; }
+        location /clouds_images { root /var/www/igor/; }
+
+        location / {
+            proxy_pass         http://127.0.0.1:8002;
+
+            proxy_set_header   Host             $host;
+            proxy_set_header   X-Real-IP        $remote_addr;
+            proxy_set_header   X-Forwarded-For  $remote_addr;
+
+            client_max_body_size       10m;
+            client_body_buffer_size    128k;
+
+            proxy_connect_timeout      90;
+            proxy_send_timeout         90;
+            proxy_read_timeout         90;
+
+            proxy_buffer_size          4k;
+            proxy_buffers              4 32k;
+            proxy_busy_buffers_size    64k;
+            proxy_temp_file_write_size 64k;
+
+            expires                    off;
+        }
+    }
+
+
---
+++ bin/srv.py
@@ -0,0 +1,261 @@
+import sys
+import logging
+import os
+import re
+import requests
+import socket
+import subprocess
+import time
+import traceback
+
+import geoip2.database
+from geopy.geocoders import Nominatim
+import jinja2
+
+import gevent
+from gevent.wsgi import WSGIServer
+from gevent.queue import Queue
+from gevent.monkey import patch_all
+from gevent.subprocess import Popen, PIPE, STDOUT
+patch_all()
+
+from flask import Flask, request, render_template, send_from_directory
+app = Flask(__name__)
+
+MYDIR = os.path.abspath(os.path.dirname( os.path.dirname('__file__') ))
+GEOLITE = os.path.join( MYDIR, "GeoLite2-City.mmdb" )
+WEGO = "/home/igor/go/bin/wego"
+
+CACHEDIR  = os.path.join( MYDIR, "cache" )
+IP2LCACHE = os.path.join( MYDIR, "cache/ip2l" )
+ALIASES   = os.path.join( MYDIR, "share/aliases" )
+ANSI2HTML = os.path.join( MYDIR, "share/ansi2html.sh" )
+HELP_FILE = os.path.join( MYDIR, 'share/help.txt' )
+LOG_FILE  = os.path.join( MYDIR, 'log/main.log' )
+TEMPLATES = os.path.join( MYDIR, 'share/templates' )
+STATIC    = os.path.join( MYDIR, 'share/static' )
+
+NOT_FOUND_LOCATION = "NOT_FOUND"
+DEFAULT_LOCATION = "Oymyakon"
+
+NOT_FOUND_MESSAGE = """
+We were unable to find your location,
+so we have brought you to Oymyakon,
+one of the coldest permanently inhabited locales on the planet.
+"""
+
+logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
+
+reader = geoip2.database.Reader(GEOLITE)
+geolocator = Nominatim()
+
+my_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader(TEMPLATES),
+])
+app.jinja_loader = my_loader
+
+
+class Limits:
+    def __init__( self ):
+        self.intervals = [ 'min', 'hour', 'day' ]
+        self.divisor = {
+            'min':      60,
+            'hour':     3600,
+            'day':      86400,
+            }
+        self.counter = {
+            'min':      {},
+            'hour':     {},
+            'day':      {},  
+            }
+        self.limit = {
+            'min':      10,
+            'hour':     20,
+            'day':      100,
+            }
+        self.last_update = {
+            'min':      0,
+            'hour':     0,
+            'day':      0,
+            }
+        self.clear_counters()
+
+    def check_ip( self, ip ):
+        self.clear_counters()
+        for interval in self.intervals:
+            if ip not in self.counter[interval]:
+                self.counter[interval][ip] = 0
+            self.counter[interval][ip] += 1
+            if self.limit[interval] <= self.counter[interval][ip]:
+                log("Too many queries: %s in %s for %s" % (self.limit[interval], interval, ip) )
+                raise RuntimeError("Not so fast! Number of queries per %s is limited to %s" % (interval, self.limit[interval]))
+            print self.counter
+
+    def clear_counters( self ):
+        t = int( time.time() )
+        for interval in self.intervals:
+            if t / self.divisor[interval] != self.last_update[interval]:
+                self.counter[interval] = {}
+                self.last_update[interval] = t / self.divisor[interval]
+        
+
+limits = Limits()
+
+def error( text ):
+    print text
+    raise RuntimeError(text)
+
+def log( text ):
+    print text
+    logging.info( text )
+
+def is_ip( ip ):
+    if re.match('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip) is None:
+        return False
+    try:
+        socket.inet_aton(ip)
+        return True
+    except socket.error:
+        return False
+
+def save_weather_data( location, filename ):
+
+    if location == NOT_FOUND_LOCATION:
+        location_not_found = True
+        location = DEFAULT_LOCATION
+    else:
+        location_not_found = False
+    
+    p = Popen( [ WEGO, '--city=%s' % location ], stdout=PIPE, stderr=PIPE )
+    stdout, stderr = p.communicate()
+    if p.returncode != 0:
+        error( stdout + stderr )
+
+    dirname = os.path.dirname( filename )
+    if not os.path.exists( dirname ):
+        os.makedirs( dirname )
+    
+    if location_not_found:
+        stdout += NOT_FOUND_MESSAGE
+
+    open( filename, 'w' ).write( stdout )
+
+    p = Popen( [ "bash", ANSI2HTML, "--palette=solarized", "--bg=dark" ],  stdin=PIPE, stdout=PIPE, stderr=PIPE )
+    stdout, stderr = p.communicate( stdout )
+    if p.returncode != 0:
+        error( stdout + stderr )
+
+    open( filename+'.html', 'w' ).write( stdout )
+
+def get_filename( location ):
+    location = location.replace('/', '_')
+    timestamp = time.strftime( "%Y%m%d%H", time.localtime() )
+    return "%s/%s/%s" % ( CACHEDIR, location, timestamp )
+
+def get_wetter(location, ip, html=False):
+    filename = get_filename( location )
+    if not os.path.exists( filename ):
+        limits.check_ip( ip )
+        save_weather_data( location, filename )
+    if html:
+        filename += '.html'
+    return open(filename).read()
+
+
+def ip2location( ip ):
+    cached = os.path.join( IP2LCACHE, ip )
+
+    if os.path.exists( cached ):
+        return open( cached, 'r' ).read()
+
+    try:
+        t = requests.get( 'http://api.ip2location.com/?ip=%s&key=demo&package=WS10' % ip ).text
+        if ';' in t:
+            location = t.split(';')[3]
+            if not os.path.exists( IP2LCACHE ):
+                os.makedirs( IP2LCACHE )
+            open( cached, 'w' ).write( location )
+            return location
+    except:
+            pass
+
+def get_location( ip_addr ):
+    response = reader.city( ip_addr )
+    city = response.city.name
+    if city is None and response.location:
+        coord = "%s, %s" % (response.location.latitude, response.location.longitude)
+        location = geolocator.reverse(coord, language='en')
+        city = location.raw.get('address', {}).get('city')
+    if city is None:
+        print ip_addr
+        city = ip2location( ip_addr )
+    return city or NOT_FOUND_LOCATION
+
+def load_aliases( aliases_filename ):
+    aliases_db = {}
+    with open( aliases_filename, 'r' ) as f:
+        for line in f.readlines():
+            from_, to_ = line.split(':', 1)
+            aliases_db[ from_.strip().lower() ] = to_.strip()
+    return aliases_db
+
+location_alias = load_aliases( ALIASES )
+def location_canonical_name( location ):
+    if location.lower() in location_alias:
+        return location_alias[location.lower()]
+    return location
+
+def show_help():
+    return open(HELP_FILE, 'r').read()
+
+@app.route('/files/<path:path>')
+def send_static(path):
+    print path
+    print STATIC
+    return send_from_directory(STATIC, path)
+
+@app.route("/")
+@app.route("/<string:location>")
+def wttr(location = None):
+    user_agent = request.headers.get('User-Agent').lower()
+
+    html_output = True
+    if 'curl' in user_agent or 'wget' in user_agent:
+        html_output = False
+
+    if location == ':help':
+        help_ = show_help()
+        if html_output:
+            return render_template( 'index.html', body=help_ )
+        else:
+            return help_
+
+    orig_location = location
+
+    if request.headers.getlist("X-Forwarded-For"):
+       ip = request.headers.getlist("X-Forwarded-For")[0]
+       if ip.startswith('::ffff:'):
+           ip = ip[7:]
+    else:
+       ip = request.remote_addr
+
+    try:
+        if location is None:
+            location = get_location( ip )
+
+        if is_ip( location ):
+            location = get_location( location )
+        if location.startswith('@'):
+            location = get_location( socket.gethostbyname( location[1:] ) )
+
+        location = location_canonical_name( location )
+        log("%s %s %s %s" % (ip, user_agent, orig_location, location))
+        return get_wetter( location, ip, html=html_output )
+    except Exception, e:
+        logging.error("Exception has occured", exc_info=1)
+        return str(e).rstrip()+"\n"
+
+server = WSGIServer(("", 8002), app)
+server.serve_forever()
+
