0ac8c3b86e75a1c312a6a5d824715cce38975c8d
Igor Chubin <igor@chub.in>
2017-03-12 17:42:22 +0100

    parse_query.py: added lib/parse_query.py

---
+++ lib/parse_query.py
@@ -0,0 +1,54 @@
+
+def parse_query(args):
+    result = {}
+
+    reserved_args = ["lang"]
+    #q = "&".join(x for x in args.keys() if x not in reserved_args)
+
+    q = ""
+
+    for key, val in args.items():
+        if len(val) == 0:
+            q += key
+            continue
+        if val == 'True':
+            val = True
+        if val == 'False':
+            val = False
+        result[key] = val
+
+    if q is None:
+        return result
+    if 'n' in q:
+        result['narrow'] = True
+    if 'm' in q:
+        result['use_metric'] = True
+    if 'u' in q:
+        result['use_imperial'] = True
+    if 'I' in q:
+        result['inverted_colors'] = True
+    if 't' in q:
+        result['transparency'] = '150'
+    if 'p' in q:
+        result['padding'] = True
+
+    for days in "0123":
+        if days in q:
+            result['days'] = days
+
+    result['no-caption'] = False
+    result['no-city'] = False
+    if 'q' in q:
+        result['no-caption'] = True
+    if 'Q' in q:
+        result['no-city'] = True
+
+    for key, val in args.items():
+        if val == 'True':
+            val = True
+        if val == 'False':
+            val = False
+        result[key] = val
+
+    return result
+
