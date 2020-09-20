4a980be2f379990e07b147ae1b7898b49b8a1c1d
Igor Chubin <igor@chub.in>
2020-09-20 09:58:55 +0200

    se pyphoon from PATH

--- lib/globals.py
+++ lib/globals.py
@@ -24,7 +24,7 @@ else:
     GEOLITE = os.path.join(MYDIR, 'data', "GeoLite2-City.mmdb")
 
 WEGO = os.environ.get("WTTR_WEGO", "/home/igor/go/bin/we-lang")
-PYPHOON = "/home/igor/pyphoon/bin/pyphoon-lolcat"
+PYPHOON = "pyphoon-lolcat"
 
 _DATADIR = "/wttr.in"
 _LOGDIR = "/wttr.in/log"
