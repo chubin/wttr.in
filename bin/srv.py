74283de5f5a677b7ee039c395b8d2c3e93ed8d44
Igor Chubin <igor@chub.in>
2020-11-07 19:57:30 +0100

    Bind v3

--- bin/srv.py
+++ bin/srv.py
@@ -10,7 +10,7 @@ import sys
 import os
 import jinja2
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, send_file
 APP = Flask(__name__)
 
 MYDIR = os.path.abspath(
@@ -21,6 +21,7 @@ import wttr_srv
 from globals import TEMPLATES, STATIC, LISTEN_HOST, LISTEN_PORT
 # pylint: enable=wrong-import-position,wrong-import-order
 
+from view.v3 import v3_file
 
 MY_LOADER = jinja2.ChoiceLoader([
     APP.jinja_loader,
@@ -28,6 +29,13 @@ MY_LOADER = jinja2.ChoiceLoader([
 ])
 
 APP.jinja_loader = MY_LOADER
+
+@APP.route('/v3/<string:location>')
+def send_v3(location):
+    filepath = v3_file(location)
+    if filepath.startswith("ERROR"):
+        return filepath.rstrip("\n") + "\n"
+    return send_file(filepath)
 
 @APP.route('/files/<path:path>')
 def send_static(path):
