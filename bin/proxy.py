0be26694127524f2a2c0c03e0557ec743b83bdfc
Igor Chubin <igor@chub.in>
2022-10-25 19:21:11 +0200

    Increase expiration interval

--- bin/proxy.py
+++ bin/proxy.py
@@ -93,7 +93,7 @@ def _cache_file(path, query):
 
     digest = hashlib.sha1(("%s %s" % (path, query)).encode("utf-8")).hexdigest()
     digest_number = ord(digest[0].upper())
-    expiry_interval = 60*(digest_number+90)
+    expiry_interval = 60*(digest_number+180)
 
     timestamp = "%010d" % (int(time.time())//expiry_interval*expiry_interval)
     filename = os.path.join(PROXY_CACHEDIR, timestamp, path, query)
