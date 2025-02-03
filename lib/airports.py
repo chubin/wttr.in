fb3ff589ad95f5454e0d10c23ba09b07078909a9
Igor Chubin <igor@chub.in>
2025-02-03 14:04:50 +0100

    Fix formatting of lib/airports.py

--- lib/airports.py
+++ lib/airports.py
@@ -1,6 +1,7 @@
 import csv
 
-AIRPORTS_DAT_FILE = '/home/igor/wttrin-geo/share/airports.dat'
+AIRPORTS_DAT_FILE = "/home/igor/wttrin-geo/share/airports.dat"
+
 
 def load_aiports_index():
     file_ = open(AIRPORTS_DAT_FILE, "r")
@@ -12,11 +13,12 @@ def load_aiports_index():
 
     return airport_index
 
+
 AIRPORTS_INDEX = load_aiports_index()
+
 
 def get_airport_gps_location(iata_code):
     if iata_code in AIRPORTS_INDEX:
         airport = AIRPORTS_INDEX[iata_code]
-        return '%s,%s airport' % (airport[6], airport[7]) #, airport[1])
+        return "%s,%s airport" % (airport[6], airport[7])  # , airport[1])
     return None
-
