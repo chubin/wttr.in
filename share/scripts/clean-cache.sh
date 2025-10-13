c93358b02ac6a529d72d87aa99ba9bebfcc354bd
Igor Chubin <igor@chub.in>
2025-10-13 15:17:46 +0200

    Improve cache cleanup script

--- share/scripts/clean-cache.sh
+++ share/scripts/clean-cache.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
+LOGFILE=/tmp/clean-cache.log
+
+_log() {
+  echo "$(date +"[%Y-%m-%d %H:%M:%S]") $*" >> "$LOGFILE"
+}
+
+_log_pipe() {
+  while read -r line; do
+    _log "$line"
+  done
+}
+
 CACHEDIR="/wttr.in/cache"
 
-for dir in wego proxy-wwo png
+_log Cleaning up the cache.
+_log Before:
+df -h "$CACHEDIR" | _log_pipe
+
+for dir in wego proxy-wwo png lru
 do
   mv "${CACHEDIR}/${dir}" "${CACHEDIR}/${dir}.old"
   mkdir "${CACHEDIR}/${dir}"
   rm -rf "${CACHEDIR}/${dir}.old"
 done
+
+_log After:
+df -h "$CACHEDIR" | _log_pipe
+_log "============================="
 
 cd /wttr.in/log
 mv main.log main.log.1
