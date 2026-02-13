82de7e0f6d14cff6e2f967e280375c90e97ff5c8
Igor Chubin <igor@chub.in>
2026-02-13 19:59:45 +0100

    Add trivial server

--- main.go
+++ main.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/chubin/wttr.go/internal/generate"
+	"github.com/chubin/wttr.go/internal/pipeline"
 )
 
+func srv() {
+	// Define routes
+	http.HandleFunc("/", pipeline.WeatherHandler)
+
+	// Start the server
+	port := ":8080"
+	log.Printf("Server starting on port %s", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	err := generate.GenerateOptions()
-	if err != nil {
-		fmt.Println(err)
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: CMD {gen|srv}")
+		os.Exit(0)
+	}
+
+	if os.Args[1] == "srv" {
+		srv()
+	}
+	if os.Args[1] == "gen" {
+		err := generate.GenerateOptions()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
