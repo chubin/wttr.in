c26301c075d3100d886b24128a6aec6612817895
Igor Chubin <igor@chub.in>
2026-03-06 12:12:06 +0100

    Improve main initialization

--- main.go
+++ main.go
@@ -7,15 +7,50 @@ import (
 	"os"
 
 	"github.com/chubin/wttr.go/internal/generate"
+	"github.com/chubin/wttr.go/internal/location"
+	"github.com/chubin/wttr.go/internal/options"
 	"github.com/chubin/wttr.go/internal/pipeline"
 )
 
 func srv() {
+	locationCache, err := location.NewCache(
+		&location.Config{
+			LocationCacheType: "",
+			LocationCacheDB:   "",
+			LocationCache:     "",
+			IPCacheType:       "",
+			NominatimServers: []struct {
+				Name  string
+				Type  string
+				URL   string
+				Token string
+			}{
+				{
+					"",
+					"",
+					"",
+					"",
+				},
+			},
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	wttrInOptions, err := options.NewFromFile("spec/options/options.yaml")
+	if err != nil {
+		log.Fatalln("error loading wttr.in options description: ", err)
+	}
+
 	ws := pipeline.NewWeatherService(
-		nil,
-		pipeline.NewCacheLocator(nil),
+		pipeline.NewWeatherClient(fmt.Sprintf(
+			"http://127.0.0.1:5001/premium/v1/weather.ashx?key=%s&q={lat},{long}&format=json&num_of_days=3&includelocation=yes",
+			os.Getenv("PROXY_KEY"),
+		)),
+		pipeline.NewCacheLocator(locationCache),
 		pipeline.NewIPCacheLocator(nil),
-		nil,
+		pipeline.NewQueryParser(wttrInOptions),
 	)
 
 	// Define routes
