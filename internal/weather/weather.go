fcc1567bea37c215370af2f417259a82c8773d86
Igor Chubin <igor@chub.in>
2026-03-11 15:00:32 +0100

    Remove excessive debug

--- internal/weather/weather.go
+++ internal/weather/weather.go
@@ -336,9 +336,7 @@ func (s *WeatherService) computeResponse(
 	// We also have information about the IP and Geolocation,
 	// which can be added to the headers.
 	// ...
-	log.Println("accessing uplink")
 	isUpstream, uplinkResponse, err := s.UplinkProcessor.Route(opts, r, ipData, location)
-	log.Println("done")
 	if isUpstream {
 		return uplinkResponse, err
 	}
