44ff455eed74339b28870cb9fb395c257f536049
Igor Chubin <igor@chub.in>
2026-03-21 15:29:55 +0100

    Fix j1 format

--- internal/weather/renderer_j1.go
+++ internal/weather/renderer_j1.go
@@ -1,6 +1,9 @@
 package weather
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type J1Renderer struct{}
 
@@ -9,8 +12,22 @@ func (r *J1Renderer) Render(query Query) (RenderOutput, error) {
 		return RenderOutput{}, fmt.Errorf("no weather data provided")
 	}
 
+	var data struct {
+		Data interface{} `json:"data"`
+	}
+
+	err := json.Unmarshal(*query.Weather, &data)
+	if err != nil {
+		return RenderOutput{}, fmt.Errorf("invalid data format")
+	}
+
+	dataBytes, err := json.MarshalIndent(data.Data, "", "  ")
+	if err != nil {
+		return RenderOutput{}, fmt.Errorf("invalid data format")
+	}
+
 	// Return the raw weather data as JSON bytes
 	return RenderOutput{
-		Content: *query.Weather,
+		Content: dataBytes,
 	}, nil
 }
