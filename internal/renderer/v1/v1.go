4e8a609c7424d76f2fe9ab224f08477d199e7717
Igor Chubin <igor@chub.in>
2026-04-02 16:51:16 +0200

    Add internal/renderer/v1/v1.go with main types definitions

---
+++ internal/renderer/v1/v1.go
@@ -0,0 +1,15 @@
+package v1
+
+type Configuration struct {
+	APIKey       string
+	City         string
+	Numdays      int
+	Imperial     bool
+	WindUnit     bool
+	Inverse      bool
+	Lang         string
+	Narrow       bool
+	LocationName string
+	WindMS       bool
+	RightToLeft  bool
+}
