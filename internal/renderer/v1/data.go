477a436494b6158f29686f2cd28bce3e6ce68262
Igor Chubin <igor@chub.in>
2026-04-03 16:15:16 +0200

    Add internal/renderer/v1/data.go

---
+++ internal/renderer/v1/data.go
@@ -0,0 +1,45 @@
+package v1
+
+type cond struct {
+	ChanceOfRain   string  `json:"chanceofrain"`
+	FeelsLikeC     int     `json:",string"`
+	PrecipMM       float32 `json:"precipMM,string"`
+	TempC          int     `json:"tempC,string"`
+	TempC2         int     `json:"temp_C,string"`
+	Time           int     `json:"time,string"`
+	VisibleDistKM  int     `json:"visibility,string"`
+	WeatherCode    int     `json:"weatherCode,string"`
+	WeatherDesc    []struct{ Value string }
+	WindGustKmph   int `json:",string"`
+	Winddir16Point string
+	WindspeedKmph  int `json:"windspeedKmph,string"`
+}
+
+type astro struct {
+	Moonrise string
+	Moonset  string
+	Sunrise  string
+	Sunset   string
+}
+
+type weather struct {
+	Astronomy []astro
+	Date      string
+	Hourly    []cond
+	MaxtempC  int `json:"maxtempC,string"`
+	MintempC  int `json:"mintempC,string"`
+}
+
+type loc struct {
+	Query string `json:"query"`
+	Type  string `json:"type"`
+}
+
+type resp struct {
+	Data struct {
+		Cur     []cond                 `json:"current_condition"`
+		Err     []struct{ Msg string } `json:"error"`
+		Req     []loc                  `json:"request"`
+		Weather []weather              `json:"weather"`
+	} `json:"data"`
+}
