c0b16b397b84d481b8b239ddc75cbe8894e80698
Igor Chubin <igor@chub.in>
2026-04-29 17:52:34 +0200

    Fix wind block (#1222, #1225)

--- internal/renderer/v2/block_wind.go
+++ internal/renderer/v2/block_wind.go
@@ -1,48 +1,96 @@
+// internal/renderer/v2/block_wind.go
 package v2
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	"github.com/chubin/wttr.in/internal/options"
 )
 
+// Wind direction constants from lib/constants.py
+var windDirectionSymbols = []string{
+	"↓", "↙", "←", "↖", "↑", "↗", "→", "↘",
+}
+
+var windDirectionSymbolsWI = []string{
+	"", "", "", "", "", "", "", "",
+}
+
+// Color thresholds exactly as in Python draw_wind
+var windColorThresholds = []struct {
+	speed int
+	color int
+}{
+	{3, 241},
+	{6, 242},
+	{9, 243},
+	{12, 246},
+	{15, 250},
+	{19, 253},
+	{23, 214},
+	{27, 208},
+	{31, 202},
+	{-1, 196},
+}
+
+func colorCodeForWindSpeed(speed int) string {
+	for _, t := range windColorThresholds {
+		if speed <= t.speed {
+			return fmt.Sprintf("38;5;%d", t.color)
+		}
+	}
+	return "38;5;196"
+}
+
+// drawWind renders the two-line wind block (exactly like Python)
 func drawWind(dirs []int, speeds []float64, opts *options.Options) string {
+	if len(dirs) == 0 || len(speeds) == 0 {
+		return "\n\n"
+	}
+
+	// Choose symbol set based on view (matches Python logic)
+	useWI := opts.View == "v2n" || opts.View == "v2d"
+	var symbols []string
+	if useWI {
+		symbols = windDirectionSymbolsWI
+	} else {
+		symbols = windDirectionSymbols
+	}
+
 	var dirLine, speedLine strings.Builder
 
-	for i, deg := range dirs {
-		dirSymbol := getWindDirection(deg)
-		color := getWindColor(speeds[i])
+	for i := range dirs {
+		degree := dirs[i]
+		speed := int(speeds[i])
 
-		dirLine.WriteString(fmt.Sprintf(" %s ", colorize(dirSymbol, color)))
+		// Direction symbol
+		var symbol string
+		if degree != 0 {
+			// Exact same formula as Python: (degree + 22.5) % 360 / 45
+			angle := float64(degree) + 22.5
+			mod := math.Mod(angle, 360)
+			idx := int(mod / 45) % 8
+			symbol = symbols[idx]
+		} else {
+			symbol = " "
+		}
 
-		spd := int(speeds[i])
-		speedStr := fmt.Sprintf("%d", spd)
-		if spd < 10 {
+		color := colorCodeForWindSpeed(speed)
+
+		// Top line: direction with spaces
+		dirLine.WriteString(fmt.Sprintf(" %s ", colorize(symbol, color)))
+
+		// Bottom line: speed with padding (exact Python logic)
+		speedStr := fmt.Sprintf("%d", speed)
+		if speed < 10 {
 			speedStr = " " + speedStr + " "
+		} else if speed < 100 {
+			speedStr = " " + speedStr
 		}
 		speedLine.WriteString(colorize(speedStr, color))
 	}
 
-	dirLine.WriteRune('\n')
-	speedLine.WriteRune('\n')
-	return dirLine.String() + speedLine.String()
-}
-
-func getWindDirection(deg int) string {
-	dirs := []string{"N", "NE", "E", "SE", "S", "SW", "W", "NW"}
-	return dirs[(deg+22)%360/45]
-}
-
-func getWindColor(speed float64) string {
-	switch {
-	case speed < 5:
-		return "38;5;242"
-	case speed < 15:
-		return "38;5;250"
-	case speed < 25:
-		return "38;5;226"
-	default:
-		return "38;5;196"
-	}
+	return dirLine.String() + "\n" + speedLine.String() + "\n"
 }
